5081b7c8aef6803d999fb2c89e160c6db23b5a4a
Adriaan de Haan <adriaan.dehaan@gmail.com>
2025-12-27 16:16:50 -0300

    fix: remove AuthWrapper import causing build failure

--- apps/frontend/src/renderer/main.tsx
+++ apps/frontend/src/renderer/main.tsx
@@ -4,13 +4,10 @@ import '../shared/i18n';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { AuthWrapper } from './components/auth/AuthWrapper';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthWrapper>
-      <App />
-    </AuthWrapper>
+    <App />
   </React.StrictMode>
 );
